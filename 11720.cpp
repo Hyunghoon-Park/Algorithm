738d1528d33066458acc27f77bcba7c29d925f09
Hyunghoon-Park <48191966+Hyunghoon-Park@users.noreply.github.com>
2020-07-19 22:27:41 +0900

    Add files via upload
    11720

---
+++ 11720.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int num;	//input num count
+	int sum = 0;	//input num's sum
+	char ch;	// input num
+	cin >> num;
+	for (int i = 0; i < num; i++) {
+		cin >> ch;
+		sum += (ch - '0');
+	}
+	cout << sum << endl;
+	return 0;
+}
