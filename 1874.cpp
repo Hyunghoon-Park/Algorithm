5f44eb0ec82c7ec2e174002039cf495b9f137102
Hyunghoon-Park <48191966+Hyunghoon-Park@users.noreply.github.com>
2020-08-25 00:02:21 +0900

    baekjoon_1874

---
+++ 1874.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+stack<int> s;
+queue<int> q;
+vector<char> v;
+
+
+int main() {
+	int n, tmp;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &tmp);
+		q.push(tmp);
+	}
+	tmp = 0;
+	for (int i = 1; i <= n; i++) {
+		v.push_back('+');
+		s.push(i);
+		if (q.front() == s.top()) {
+			while (s.size() > 0 && q.front() == s.top()) {
+				v.push_back('-');
+				s.pop();
+				q.pop();
+			}
+		}
+	}
+	if (s.size() != 0) {
+		printf("NO");
+	}
+	else {
+		for (int i = 0; i < v.size(); i++) {
+			printf("%c\n", v[i]);
+		}
+	}
+
+	return 0;
+}
