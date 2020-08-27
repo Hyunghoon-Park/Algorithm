7915bc2044d79fe9b168ba1fee588810833db4dd
Hyunghoon-Park <gudgns0@gmail.com>
2020-08-27 21:50:16 +0900

    baekjoon_2667

---
+++ 2667.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+vector<vector<int>> v;
+vector<vector<bool>> visit;
+int dx[4] = {-1,1,0,0};
+int dy[4] = {0,0,-1,1};
+int n, tmp, range;
+
+void bfs(int x, int y);
+int main() {
+	scanf("%d", &n);
+	v.resize(n+2);
+	visit.resize(n+2);
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			scanf("%d", &tmp);
+			v[i].push_back(tmp);
+		}
+	}
+	tmp = 0;
+	bfs(1,1);
+	return 0;
+}
+
+void bfs(int x, int y){
+	
+}
