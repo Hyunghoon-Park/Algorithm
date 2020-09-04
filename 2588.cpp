3df8df453aa09c408abf6ab36019888f8f6cdc5a
Hyunghoon-Park <48191966+Hyunghoon-Park@users.noreply.github.com>
2020-09-05 00:08:16 +0900

    baekjoon_2588.cpp

--- 2588.cpp
+++ 2588.cpp
@@ -4,12 +4,9 @@ using namespace std;
 int main(void){
 	int a, b, c = -1, sum = 0;
 	int ten = 10;
-    
 	cin >> a >> b;
-    
 	for(int i = 0; i < 3; i++) {
 		c = b % ten;
-        
 		cout << a * c * 10 /ten << endl;
         
         sum += a * c;
