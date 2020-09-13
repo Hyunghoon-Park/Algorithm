73bbd8bc9ef8ad2a5df183c6dce2f4e8443aed03
Hyunghoon-Park <gudgns0@gmail.com>
2020-09-13 19:22:17 +0900

    baekjoon_2748

---
+++ 2748.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<long long> v;
+
+int main(){
+    int n;
+    v.push_back(0);
+    v.push_back(1);
+    scanf("%d", &n);
+    for(int i = 2; i <= n; i++){
+        v.push_back(v[i-1] + v[i-2]);
+    }
+    printf("%lld", v[n]);
+    return 0;
+}
