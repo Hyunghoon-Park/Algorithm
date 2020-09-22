965d7d9b017b05f3e5f990f4fa7c805334459021
Hyunghoon-Park <gudgns0@gmail.com>
2020-09-22 17:43:32 +0900

    baekjoon_10844

---
+++ 10844.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[101][10];
+int main(){
+    int n, result = 0;
+    scanf("%d", &n);
+    dp[1][0] = 0;
+    for(int i = 1; i <= 9; i++){
+        dp[1][i] = 1;
+    }
+    for(int i = 2; i <= n; i++){
+        for(int j = 0; j < 10; j++){
+            if(j == 0){
+                dp[i][j] = dp[i-1][j+1];
+            }
+            else if(j == 9){
+                dp[i][j] = dp[i-1][j-1];
+            }
+            else{
+                dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % 1000000000;
+            }
+        }
+    }
+    for(int i = 0; i < 10; i++){
+        result = (result + dp[n][i]) % 1000000000;
+    }
+    printf("%d", result);
+    return 0;
+}
