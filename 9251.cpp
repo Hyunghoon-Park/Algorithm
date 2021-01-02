90789c857ba70607d630989934dc57c3e90cc12c
Hyunghoon-Park <gudgns0@gmail.com>
2021-01-03 01:04:09 +0900

    baekjoon_9251

--- 9251.cpp
+++ 9251.cpp
@@ -9,8 +9,7 @@ int main() {
     scanf("%s %s", s1 + 1, s2 + 1);
     for (i = 1; s1[i]; i++)
         for (j = 1; s2[j]; j++)
-            dp[i][j] = max({ dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1] + (s1[i] == s2[j]) 
-                           });
+            dp[i][j] = max({ dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1] + (s1[i] == s2[j]) });
     printf("%d", dp[i - 1][j - 1]);
     return 0;
 }
