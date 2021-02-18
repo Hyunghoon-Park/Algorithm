fce519e95bda8e1c6743907acf8dcfaf3703b9ee
Hyunghoon-Park <gudgns0@gmail.com>
2021-02-18 16:58:26 +0900

    baekjoon_1149

--- 1149.cpp
+++ 1149.cpp
@@ -8,9 +8,8 @@ int dp[1000][3] = {0};
 int main(){
     int n;
     scanf("%d",&n);
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++)
         scanf("%d %d %d", &dp[i][0], &dp[i][1], &dp[i][2]);
-    }
     for(int i = 1; i < n; i++){
         dp[i][0] = min(dp[i][0] + dp[i-1][1], dp[i][0] + dp[i-1][2]);
         dp[i][1] = min(dp[i][1] + dp[i-1][0], dp[i][1] + dp[i-1][2]);
