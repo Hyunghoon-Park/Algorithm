c9ad531761d899dd3b93b5ebb1d8964f9fad8dfc
Hyunghoon-Park <gudgns0@gmail.com>
2021-02-18 16:58:00 +0900

    baekjoon_1932

--- 1932.cpp
+++ 1932.cpp
@@ -6,11 +6,9 @@ int tri[501][501] = {0};
 int main(){
     int n, result = 0;
     scanf("%d", &n);
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= i; j++){
-            scanf("%d", &tri[i][j]);
-        }
-    }
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= i; j++)
+            scanf("%d", &tri[i][j]); 
     for(int i = 2; i <= n; i++)
         for(int j = 1; j <= i; j++)
            tri[i][j] = max(tri[i-1][j-1] + tri[i][j], tri[i-1][j] + tri[i][j]);
